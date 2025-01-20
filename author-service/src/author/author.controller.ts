fb657061eaa1f9383f7ac9a3f010dd536e8abe3b
David Arinze <fivelanes72@gmail.com>
2025-01-20 21:13:37 +0100

    feat: Set up Author and Book services with Prisma for CRUD operations
    - Generated author and book modules for crud operations
    - Removed unnecessary entities folder as Prisma models are used for database schema
    - Set up .env and PostgreSQL database connection for both services

---
+++ author-service/src/author/author.controller.ts
@@ -0,0 +1,35 @@
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { AuthorService } from './author.service';
+import { CreateAuthorDto } from './dto/create-author.dto';
+import { UpdateAuthorDto } from './dto/update-author.dto';
+
+@Controller()
+export class AuthorController {
+  constructor(private readonly authorService: AuthorService) {}
+
+  @MessagePattern('createAuthor')
+  create(@Payload() createAuthorDto: CreateAuthorDto) {
+    return this.authorService.create(createAuthorDto);
+  }
+
+  @MessagePattern('findAllAuthor')
+  findAll() {
+    return this.authorService.findAll();
+  }
+
+  @MessagePattern('findOneAuthor')
+  findOne(@Payload() id: number) {
+    return this.authorService.findOne(id);
+  }
+
+  @MessagePattern('updateAuthor')
+  update(@Payload() updateAuthorDto: UpdateAuthorDto) {
+    return this.authorService.update(updateAuthorDto.id, updateAuthorDto);
+  }
+
+  @MessagePattern('removeAuthor')
+  remove(@Payload() id: number) {
+    return this.authorService.remove(id);
+  }
+}
